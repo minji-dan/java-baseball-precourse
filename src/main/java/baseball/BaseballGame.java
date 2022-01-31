bc2e2778ac52ba33bf17f1b5c881d0a3b1683a31
minji-dan <aksen569@naver.com>
2022-02-01 01:30:02 +0900

    feat : BaseballGame class 생성

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,40 @@
+package baseball;
+
+import utils.NumbersFactory;
+import view.InputView;
+import view.OutputView;
+
+import java.util.Scanner;
+
+public class BaseballGame {
+    private final Scanner scanner;
+
+    private boolean finish = false;
+
+    public BaseballGame(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void play() {
+        final Numbers target = NumbersFactory.createRandomNumbers();
+
+        do {
+            Numbers guess = InputView.getNumbers(scanner);
+            Score score = calculateScore(target, guess);
+            OutputView.printScore(score);
+            checkGameOver(score);
+        } while (!finish);
+        OutputView.printGameOver();
+    }
+
+    private Score calculateScore(Numbers target, Numbers guess) {
+        int strikeCnt = GameRule.countStrike(target, guess);
+        int ballCnt = GameRule.countBall(target, guess);
+
+        return new Score(strikeCnt, ballCnt);
+    }
+
+    private void checkGameOver(Score score) {
+        finish = GameRule.checkGameEnd(score);
+    }
+}
